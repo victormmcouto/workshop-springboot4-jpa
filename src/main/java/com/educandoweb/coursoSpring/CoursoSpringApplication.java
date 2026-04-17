1a95a30f0ba38bdbf55ce8b34fc71b7154f21549
victormmcouto <myprogrammingthings@gmail.com>
2026-04-17 13:55:16 -0300

    Projeto criado

---
+++ src/main/java/com/educandoweb/coursoSpring/CoursoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/coursoSpring/CoursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
