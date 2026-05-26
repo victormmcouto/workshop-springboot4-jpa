028f60d85a43a565fcaec339dac224e2e50917a3
victormmcout <myprogrammingthings@gmail.com>
2026-05-25 21:01:28 -0300

    Create User entitie and resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.User;
+
+/*
+This is a resource class. This kind of class, in this application, serves the purpose of
+granting the interface access to the data. (REST CONTROLLER)
+ */
+
+@RestController
+@RequestMapping(value = "/users") //Defines the resource path.
+public class UserResource {
+	
+	//ending point
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1, "Maria", "maria@gmail.colm", "31972110555", "******");
+		
+		return ResponseEntity.ok().body(u);
+	}
+}
