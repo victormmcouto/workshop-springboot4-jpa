5ad2071a86e9b65d1d95e899358b955bdd3d37e3
victormmcout <myprogrammingthings@gmail.com>
2026-05-27 22:55:47 -0300

    Implements getSubTotal e getTotal methods

--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -51,7 +53,8 @@ public class Payment implements Serializable {
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
-
+	
+	@JsonIgnore
 	public Order getOrder() {
 		return order;
 	}
